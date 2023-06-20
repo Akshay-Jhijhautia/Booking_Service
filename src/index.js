15bfdd6629e46486789b0a17232b36b1de3498b5
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2023-06-20 22:37:34 +0530

    added booking model and utils from flight service

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-const express = require('express');
+const express = require("express");
 
-const { ServerConfig } = require('./config');
-const apiRoutes = require('./routes');
+const { ServerConfig } = require("./config");
+const apiRoutes = require("./routes");
 
 const app = express();
 
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Healthy");
+});
 
 app.listen(ServerConfig.PORT, () => {
-    console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+  console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
 });
