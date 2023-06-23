fa169fb3328b13e73b29e7765aef6c79784cc018
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2023-06-23 10:19:02 +0530

    added booking logic with transactions

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -6,25 +6,37 @@ const db = require("../models");
 const { ServerConfig } = require("../config");
 const AppError = require("../utils/errors/app-error");
 
+const bookingRepository = new BookingRepository();
+
 async function createBooking(data) {
-  return new Promise((resolve, reject) => {
-    // Implementing a transaction, this is a managed transaction, i.e we do no have to roll back manually
-    const result = db.sequelize.transaction(
-      async function bookingImplementation(t) {
-        const flight = await axios.get(
-          `${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`
-        );
-        const flightData = flight.data.data;
-        if (data.noOfSeats > flightData.totalSeats) {
-          // if we throw error, transaction automatically gets reverted i.e rollback
-          reject(
-            new AppError("Not enough seats available", StatusCodes.BAD_REQUEST)
-          );
-        }
-        resolve(true);
+  // this is a unmanaged transaction i.e we need to roll back manually, in case of error
+  const transaction = await db.sequelize.transaction(); // we are making a transaction object here
+  try {
+    const flight = await axios.get(
+      `${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`
+    );
+    const flightData = flight.data.data;
+    if (data.noOfSeats > flightData.totalSeats) {
+      throw new AppError("Not enough seats available", StatusCodes.BAD_REQUEST);
+    }
+
+    const totalBillingAmount = data.noOfSeats * flightData.price;
+    const bookingPayload = { ...data, totalCost: totalBillingAmount };
+    const booking = await bookingRepository.create(bookingPayload, transaction);
+
+    await axios.patch(
+      `${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}/seats`,
+      {
+        seats: data.noOfSeats,
       }
     );
-  });
+
+    await transaction.commit();
+    return booking;
+  } catch (error) {
+    await transaction.rollback();
+    throw error;
+  }
 }
 
 module.exports = {
