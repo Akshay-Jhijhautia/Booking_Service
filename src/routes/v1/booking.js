cc3056904a3d069b467a52d0b1204449533ed17d
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2023-06-23 11:28:59 +0530

    added booking payments api, with checks

--- src/routes/v1/booking.js
+++ src/routes/v1/booking.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const { BoookingController } = require("../../controllers");
 
 router.post("/", BoookingController.createBooking);
+router.post("/payments", BoookingController.makePayment);
 
 module.exports = router;
--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -5,12 +5,14 @@ const { BookingRepository } = require("../repositories");
 const db = require("../models");
 const { ServerConfig } = require("../config");
 const AppError = require("../utils/errors/app-error");
+const { Enums } = require("../utils/common");
+const { BOOKED, CANCELLED, INITIATED, PENDING } = Enums.BOOKING_STATUS;
 
 const bookingRepository = new BookingRepository();
 
 async function createBooking(data) {
   // this is a unmanaged transaction i.e we need to roll back manually, in case of error
-  const transaction = await db.sequelize.transaction(); // we are making a transaction object here
+  const transaction = await db.sequelize.transaction(); // we are making a transaction object here, all queries made after this are part of transaction
   try {
     const flight = await axios.get(
       `${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`
@@ -39,6 +41,56 @@ async function createBooking(data) {
   }
 }
 
+// A demo payment api, to unbook the seats, i.e no. of selected seats in above api, if payment is not completed by the user, or payment fails.
+async function makePayment(data) {
+  const transaction = await db.sequelize.transaction();
+  try {
+    const bookingDetails = await bookingRepository.get(
+      data.bookingId,
+      transaction
+    );
+    if (bookingDetails.status == CANCELLED) {
+      throw new AppError("The booking has expired", StatusCodes.BAD_REQUEST);
+    }
+
+    const bookingTime = new Date(bookingDetails.createdAt);
+    const currentTime = new Date();
+    // here we are checking if the payment is made within 5 minutes, after selecting/booking the seats
+    if (currentTime - bookingTime > 300000) {
+      // 5 minutes = 300000 miliseconds
+      await bookingRepository.update(data.bookingId, { status: CANCELLED });
+      throw new AppError("The booking has expired", StatusCodes.BAD_REQUEST);
+    }
+
+    if (bookingDetails.userId != data.userId) {
+      throw new AppError(
+        "User Details does not match",
+        StatusCodes.BAD_REQUEST
+      );
+    }
+    if (bookingDetails.totalCost != data.totalCost) {
+      throw new AppError(
+        "Amount of payment does not match",
+        StatusCodes.BAD_REQUEST
+      );
+    }
+
+    // we assume here that payment is successfull
+    bookingRepository.update(
+      data.bookingId,
+      {
+        status: BOOKED,
+      },
+      transaction
+    );
+    await transaction.commit();
+  } catch (error) {
+    await transaction.rollback();
+    throw error;
+  }
+}
+
 module.exports = {
   createBooking,
+  makePayment,
 };
